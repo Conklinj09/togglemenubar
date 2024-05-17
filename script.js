bbafaa029f4b2647f2ce80b33529a9cfb63932a3
Jeanette Conklin <conklinj09@gmail.com>
2024-05-17 13:10:57 -0700

    add base files

---
+++ script.js
@@ -0,0 +1,10 @@
+script.js
+
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdownContent");
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
+}
